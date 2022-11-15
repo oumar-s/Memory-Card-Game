74b010cc872290abf8c57c64cada92afe062fef2
Oumar Siby <oumarsiby10@gmail.com>
2022-11-15 08:36:45 -0500

    added return statement to the card component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Card from './Card/card';
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card text="A" />
     </div>
   );
 }
--- src/Card/card.js
+++ src/Card/card.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
+import './card.css'
 
 
 export default function Card(props){
     //state
     const [condition, setCondition] = useState({isFlipped: false});
-
-
     //onClick
 
 
+    const cardFront = 
+    <div className='front'>
+        {props.text}
+    </div>
+
+    const cardBack = 
+    <div className='back'>
+
+    </div>
     //return
-    
+    if(condition.isFlipped === false)
+        return cardBack
+    return cardFront
+
 }
