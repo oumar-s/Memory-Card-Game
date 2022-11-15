61e6282eb2794c936e6d63e5ea18547ee856e93d
Oumar Siby <oumarsiby10@gmail.com>
2022-11-15 08:15:44 -0500

    added state to the card component

---
+++ src/Card/card.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+
+export default function Card(props){
+    //state
+    const [condition, setCondition] = useState({isFlipped: false});
+
+
+    //onClick
+
+
+    //return
+    
+}
