41410a6cae8cbe4382abf227d7ef3084cd8c9b79
Oumar Siby <oumarsiby10@gmail.com>
2022-11-15 10:43:12 -0500

    added the shuffle feature to shuffle the cards

--- src/App.js
+++ src/App.js
@@ -1,55 +1,69 @@
 import './App.css';
+import React, { useState } from 'react';
 import Card from './Card/card';
 
 function App() {
-
-  const cards = [
+  const cardsList = [
     {
       id: 1,
       content: "A",
       isMatched: false
     },
     {
-      id: 1,
+      id: 2,
       content: "A",
       isMatched: false
     },
     {
-      id: 1,
+      id: 3,
       content: "B",
       isMatched: false
     },
     {
-      id: 1,
+      id: 4,
       content: "B",
       isMatched: false
     },
     {
-      id: 1,
+      id: 5,
       content: "C",
       isMatched: false
     },
     {
-      id: 1,
+      id: 6,
       content: "C",
       isMatched: false
     },
     {
-      id: 1,
+      id: 7,
       content: "D",
       isMatched: false
     },
     {
-      id: 1,
+      id: 8,
       content: "D",
       isMatched: false
     }
   ]
+
+  /* Randomize array in-place using Durstenfeld shuffle algorithm */
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+ }
+  shuffleArray(cardsList);
+
+  const [cards, setCards] = useState(cardsList)
+  
   return (
     <div className="App">
       <h1>Memory Game</h1>
       <button>New Game</button>
-      {cards.map((card,idx)=>(<Card key={card.id} card={card}/>))}
+      <div className='card'>
+        {cards.map((card,idx)=>(<Card key={card.id} card={card}/>))}
+      </div>
     </div>
   );
 }
